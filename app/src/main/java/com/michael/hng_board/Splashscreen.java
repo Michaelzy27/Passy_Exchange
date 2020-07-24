a6284ef763143a947e9bd67dcafbe44d3893651c
Alabi <alabibolaji78@gmail.com>
2020-07-24 14:09:37 +0100

    fixed

--- app/src/main/java/com/michael/hng_board/Splashscreen.java
+++ app/src/main/java/com/michael/hng_board/Splashscreen.java
@@ -25,7 +25,6 @@ public class Splashscreen extends AppCompatActivity {
             @Override
             public void run() {
                 Toast.makeText(Splashscreen.this, "Splashscreen done", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(Splashscreen.this,SignUpActivity.class));
             }
         }, 3000);
 
