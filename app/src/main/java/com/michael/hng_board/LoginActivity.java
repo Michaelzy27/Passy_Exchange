61c20ed21644422424e3064918a5926f4bd30739
Alabi <alabibolaji78@gmail.com>
2020-07-25 20:01:12 +0100

    undid delete

--- app/src/main/java/com/michael/hng_board/LoginActivity.java
+++ app/src/main/java/com/michael/hng_board/LoginActivity.java
@@ -17,6 +17,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        signIn = findViewById(R.id.go_to_signin);
 
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
