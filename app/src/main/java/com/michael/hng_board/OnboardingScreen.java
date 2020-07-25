76f07624a735688051b8e8c46a3117e79fdbaeef
Michaelzy27 <55640053+Michaelzy27@users.noreply.github.com>
2020-07-25 15:08:48 +0100

    Sign Up API implementation
    Allow users to sign up successfully

--- app/src/main/java/com/michael/hng_board/OnboardingScreen.java
+++ app/src/main/java/com/michael/hng_board/OnboardingScreen.java
@@ -2,9 +2,18 @@ package com.michael.hng_board;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class OnboardingScreen extends AppCompatActivity {
+
+    public void toSignUp(View view) {
+
+        Intent i = new Intent(this, SignUpActivity.class);
+        startActivity(i);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
