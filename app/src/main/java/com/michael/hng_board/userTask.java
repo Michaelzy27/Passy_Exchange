7c5156a3b9780042c6a15b89013a8690c2c578ea
yetundeolubola <akinwaleyetunde1@gmail.com>
2020-07-25 13:10:51 +0100

    add user task

---
+++ app/src/main/java/com/michael/hng_board/userTask.java
@@ -0,0 +1,14 @@
+package com.michael.hng_board;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_task);
+    }
+}
