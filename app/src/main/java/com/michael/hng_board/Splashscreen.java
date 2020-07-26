db13ce58f71912b0ff3bdaf46060b512895da3f1
Adedoyin <isradeoluwa@gmail.com>
2020-07-26 23:16:32 +0100

    Intent From Splash Screen To Get Started Screen and removed the Splash Screen and Onboarding Screen from the App Stack After Opening the App

--- app/src/main/java/com/michael/hng_board/Splashscreen.java
+++ app/src/main/java/com/michael/hng_board/Splashscreen.java
@@ -24,10 +24,10 @@ public class Splashscreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Toast.makeText(Splashscreen.this, "Splashscreen done", Toast.LENGTH_SHORT).show();
-
-                Intent intent = new Intent(Splashscreen.this, LoginActivity.class);
+                //Toast.makeText(Splashscreen.this, "Splashscreen done", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Splashscreen.this, OnboardingScreen.class);
                 startActivity(intent);
+                finish();
             }
         }, 3000);
 
