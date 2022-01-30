76e6235881b0b3f1e80c63405ef140f10ef3be1c
Umeh Nnaemeka <umehmichael27@gmail.com>
2022-01-30 23:14:46 +0100

    Login cleanup

--- app/src/main/java/com/michael/passy_exchange/LoginActivity.java
+++ app/src/main/java/com/michael/passy_exchange/LoginActivity.java
@@ -59,78 +59,7 @@ public class LoginActivity extends AppCompatActivity {
         String username = Username.getText().toString();
         String password = Password.getText().toString();
 
-
-//        Login login = new Login();
-//        login.execute(username,password);
 //        helper.progressDialogStart("Logging in", "Please wait while we log you in");
 
-    }
-
-    public class Login extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... strings) {
-
-            Log.i("test", "test");
-
-            OkHttpClient okHttpClient = new OkHttpClient();
-
-            String email = strings[0];
-            String password = strings[1];
-
-
-            RequestBody requestBody = new FormBody.Builder()
-                    .add("email", email)
-                    .add("password", password)
-                    .build();
-
-            Request request = new Request.Builder().url("https://hngboard.herokuapp.com/users/login").post(requestBody).build();
-
-            try{
-                Response response = okHttpClient.newCall(request).execute();//gets a response from the server
-                Log.i("hhh", "hhh0");
-                String result = response.body().string();
-                Log.i("responseBody", result);
-                JSONObject jsonObject = new JSONObject(result);
-                String resultData = jsonObject.getString("status");
-                if (resultData.equalsIgnoreCase("success")){
-
-                    JSONObject newjsonObject = new JSONObject(result);
-                    String dataData = jsonObject.getString("data");
-                    JSONObject datajsonObject = new JSONObject(dataData);
-                    String token = datajsonObject.getString("token");
-                    String userId = datajsonObject.getString("userId");
-
-                    helper.progressDialogEnd();
-                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(i);
-                    finish();
-
-                    SharedPreferences sharedPreferences = getSharedPreferences("TOKEN", Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putString("token", token);
-                    editor.putString("userId", userId);
-                    editor.apply();
-
-                }else {
-                    helper.progressDialogEnd();
-                    showToast("Login failed! Check login details");
-                }
-
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-    }
-
-    public void showToast(final String Text){
-        this.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(LoginActivity.this, Text, Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
